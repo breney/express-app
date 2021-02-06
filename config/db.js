e7958ea9e7b1c85eeb7bae75cab3d086f9a8df08
13dev <qwerty124563@gmail.com>
2021-02-06 18:37:41 +0000

    format

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
-var connection = mongoose.connect('mongodb://localhost:27017/library_db', { useNewUrlParser: true, useUnifiedTopology: true })
+module.exports = mongoose
+    .connect('mongodb://localhost:27017/library_db', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(_ => console.log("Mongo Connected"))
     .catch(console.log);
-
-module.exports = connection;
